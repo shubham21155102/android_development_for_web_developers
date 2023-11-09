745b23c9062339beae57d91ba1ed6e126e60ec34
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-11-09 12:11:16 +0530

    err resolved

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import LandingPage from './landingPage'
+import LandingPage from "./LandingPage/index"
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
